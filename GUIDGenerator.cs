732c186c66aad191dee444339ea16995fd79ccc1
TheFirstCycleManuscript <79851719+TheFirstCycleManuscript@users.noreply.github.com>
2021-11-24 12:44:46 +0100

    Create GUIDGenerator.cs
    Committing the code.

---
+++ GUIDGenerator.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEditor;
+
+public class GUIDGenerator : EditorWindow
+{
+    private string m_GUID = "";
+
+    [MenuItem("Tools/GUID Generator")]
+    private static void Init()
+    {
+        GUIDGenerator window = (GUIDGenerator)EditorWindow.GetWindow(typeof(GUIDGenerator));
+    }
+
+    private void OnGUI()
+    {
+        m_GUID = EditorGUILayout.TextField("GUID (Unique ID):", m_GUID);
+
+        if (GUILayout.Button("Generate"))
+        {
+            m_GUID = System.Guid.NewGuid().ToString();
+        }
+    }
+}
